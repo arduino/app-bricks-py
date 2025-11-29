e980648dcb71b993e08f0e29bca351e47c4a04bc
Roberto Gazia <r.gazia@arduino.cc>
2025-11-29 16:27:04 +0100

    refactor

--- src/arduino/app_peripherals/remote_sensor/remote_sensor.py
+++ src/arduino/app_peripherals/remote_sensor/remote_sensor.py
@@ -424,7 +424,7 @@ class RemoteSensor:
             self._server_thread.join(timeout=10.0)
 
     async def _disconnect_and_stop(self):
-        """Set the async stop event and close the client connection."""
+        """Cleanly disconnect client with goodbye message and stop the server."""
         async with self._client_lock:
             if self._client:
                 try:
