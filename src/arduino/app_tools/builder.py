ad038df5d11ff286b8ac8a757f0ed0596180ee72
Marco Colombo <ing.marco.colombo@gmail.com>
2026-03-03 17:10:28 +0100

    chore: support for RC releases

--- src/arduino/app_tools/builder.py
+++ src/arduino/app_tools/builder.py
@@ -20,7 +20,7 @@ def run_preprocessing(dev_mode: bool = False) -> None:
         version = get_version(
             version_scheme="only-version",
             local_scheme="no-local-version",
-            tag_regex="^(ai|release)/(?P<version>[0-9.]+)$",
+            tag_regex="^(ai|release)/(?P<version>[0-9.]+(?:rc[0-9]+)?)$"
         )
 
     cache_folder_path = "src/arduino/app_bricks/static"
