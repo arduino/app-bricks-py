ea09cfa1e7f42c67aacbd4cf52365d4a3ba3e6e8
Marco Colombo <ing.marco.colombo@gmail.com>
2026-03-18 11:13:13 +0100

    Refactoring

---
+++ src/arduino/app_bricks/llm/examples/test.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: Copyright (C) ARDUINO SRL (http://www.arduino.cc)
+#
+# SPDX-License-Identifier: MPL-2.0
+
+# EXAMPLE_NAME = "Chat with a Local LLM"
+# EXAMPLE_REQUIRES = "Models must be downloaded and available locally."
+
+from arduino.app_bricks.llm import LargeLanguageModel
+
+llm = LargeLanguageModel()
